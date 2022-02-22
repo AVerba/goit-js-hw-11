ae7ac9aad572b3198afb80e80a86bbc4e3db33dd
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-22 19:21:09 +0200

    modify app

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,45 @@
-import { fetchImages } from "./api/apiService";
+import { imageService } from "./api/apiService";
 import { createImageCard } from "./createImageCard"; 
 import { createImageList } from "./renderImageCard";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const ref={
     imageGallery: document.querySelector('.cards-set'),
     searchForm: document.querySelector('.search-form'),
 }
 
+let lightboxGallery = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+const clearGallery=(ref)=>{
+    ref.innerHTML = '';
+  }
+  
+const smoothScroll=()=>{
+  
+      const { height: cardHeight } = document
+          .querySelector('.card')
+          .firstElementChild.getBoundingClientRect();
+  
+      console.log(cardHeight);
+      window.scrollBy({
+          top: cardHeight * 4,
+          behavior: 'smooth',
+      });  
+  }
+
+
 const searchFormHendler=(e)=>{
     e.preventDefault();
+    
     const searchValue = e.currentTarget.elements.searchQuery.value.trim();
     console.log (searchValue);
 
@@ -20,13 +51,4 @@ const searchFormHendler=(e)=>{
 
 
 ref.searchForm.addEventListener('submit', searchFormHendler)
-ref.imageGallery.addEventListener('scroll', ()=>{
-    if (ref.imageGallery.scrollTop + ref.imageGallery.clientHeight >= ref.imageGallery.scrollHeight) {
-        fetchImages('cats').then((res)=>{
-            const {hits: items}=res;
-            ref.imageGallery.innerHTML= createImageList(items);       
-        })
-      }
-
-})
 
