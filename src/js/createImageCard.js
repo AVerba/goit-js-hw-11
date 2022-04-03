74f273451366cdab6036be5c020f0da429c3ddee
Oleksii Verbytskyi <alverba88@gmail.com>
2022-04-03 09:29:32 +0300

    finished task

--- src/js/createImageCard.js
+++ src/js/createImageCard.js
@@ -6,15 +6,13 @@ export const createImageCard=(item)=>{
     return `
     <li class="card">
     <div class="card__thumb">
-      <img src="${largeImageURL}" alt="${tags}" width="450">
+      <img src="${largeImageURL}" alt="${tags}" >
     </div>
     <div class="card__content info">
       <ul class="info__list">
-        <li class="info__item">
-          <svg class="icon" width="20" height="20">
-            <!-- <use xlink:href="./images/icons/icons.svg#twitter"></use> -->
-          </svg>
-          <span class="info__text">likes ${likes}</span>
+        <li class="info__item"> 
+          <span class="info__text">like</span>
+          <b >${likes}</b>
         </li>
     
         <li class="info__item">
